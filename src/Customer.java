44daa47c41de6476eb7c6b3834e9a7d61ed38229
cauntaun <cauntaun@gmail.com>
2016-04-27 17:12:27 +0200

    Replace Temp with Query (totalAmount = getTotalCharge() )

--- src/Customer.java
+++ src/Customer.java
@@ -33,10 +33,19 @@ class Customer {
             totalAmount += each.getCharge();
         }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
+    }
 
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration enum_rentals = rentals.elements();
+        while (enum_rentals.hasMoreElements()) {
+            Rental each = (Rental) enum_rentals.nextElement();
+            result += each.getCharge();
+        }
+        return result;
     }
 
 
